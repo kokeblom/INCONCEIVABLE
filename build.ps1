ccbf7be1349f00c6a984e3c1e6d7b3df06e424c4
kokeblom <98337374+kokeblom@users.noreply.github.com>
2020-08-15 12:41:48 +0000

    New translations Resources.resx (Swedish)

--- build.ps1
+++ build.ps1
@@ -7,3 +7,5 @@ Install-package BuildUtils -Confirm:$false -Scope CurrentUser -Force
 Import-Module BuildUtils
 
 $runningDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+$nug
