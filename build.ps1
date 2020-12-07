0a7f85ff3bc1b5e8e2ea67743dc80cc1908029d7
kokeblom <98337374+kokeblom@users.noreply.github.com>
2020-12-07 19:00:29 +0000

    v0.6.5

--- build.ps1
+++ build.ps1
@@ -22,4 +22,4 @@ $assemblyFileVersion = $version.assemblyFileVersion
 $nugetPackageVersion = $version.nugetVersion
 $assemblyInformationalVersion = $version.assemblyInformationalVersion
 
-Write-host "assemblyInformationalVersion   = $assemblyInformationa
+Write-host "assemblyInformationalVersion   = $assemblyInformationalVe
