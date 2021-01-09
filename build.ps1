dc80e909598b91bde469851bf523c871ff54b0db
kokeblom <98337374+kokeblom@users.noreply.github.com>
2021-01-09 11:36:41 +0000

    Tinkering

--- build.ps1
+++ build.ps1
@@ -25,3 +25,4 @@ $assemblyInformationalVersion = $version.assemblyInformationalVersion
 Write-host "assemblyInformationalVersion   = $assemblyInformationalVersion"
 Write-host "assemblyVer                    = $assemblyVer"
 Write-host "assemblyFileVersion            = $assemblyFileVersion"
+Write-host
