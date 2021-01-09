9c2496789d73e3cf6c54da5b18a7415f9a6401f0
kokeblom <98337374+kokeblom@users.noreply.github.com>
2021-01-09 08:10:32 +0000

    coding standard

--- build.ps1
+++ build.ps1
@@ -24,4 +24,4 @@ $assemblyInformationalVersion = $version.assemblyInformationalVersion
 
 Write-host "assemblyInformationalVersion   = $assemblyInformationalVersion"
 Write-host "assemblyVer                    = $assemblyVer"
-Write-host "assemblyFileVersion            = $assemblyFi
+Write-host "assemblyFileVersion            = $assemblyFileVer
