2a1e269f57569681e3e4b3f231d00a2e20090f93
kokeblom <98337374+kokeblom@users.noreply.github.com>
2021-01-19 06:06:52 +0000

    Update documentation/SoftwareDefinedRadio.md

--- build.ps1
+++ build.ps1
@@ -25,4 +25,4 @@ $assemblyInformationalVersion = $version.assemblyInformationalVersion
 Write-host "assemblyInformationalVersion   = $assemblyInformationalVersion"
 Write-host "assemblyVer                    = $assemblyVer"
 Write-host "assemblyFileVersion            = $assemblyFileVersion"
-Write-host "nugetPackageVersion            =
+Write-host "nugetPackageVersion            = $nu
