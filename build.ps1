a38ff00a7b940df9a3dae00c2841c66821a8212f
kokeblom <98337374+kokeblom@users.noreply.github.com>
2021-01-20 16:09:48 +0000

    solve conflicts

--- build.ps1
+++ build.ps1
@@ -27,4 +27,4 @@ Write-host "assemblyVer                    = $assemblyVer"
 Write-host "assemblyFileVersion            = $assemblyFileVersion"
 Write-host "nugetPackageVersion            = $nugetPackageVersion"
 
-# Now restore packa
+# Now restore packages
