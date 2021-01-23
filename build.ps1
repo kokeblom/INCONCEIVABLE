cb5400556773071155422a091d4aad912eee4b7e
kokeblom <98337374+kokeblom@users.noreply.github.com>
2021-01-23 20:56:29 +0000

    Remove some warnings.

--- build.ps1
+++ build.ps1
@@ -28,4 +28,4 @@ Write-host "assemblyFileVersion            = $assemblyFileVersion"
 Write-host "nugetPackageVersion            = $nugetPackageVersion"
 
 # Now restore packages and build everything.
-Write-Hos
+Write-Host "\n
