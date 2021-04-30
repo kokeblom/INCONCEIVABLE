c443c5052b096acea36a78f9998e7ffb5ee38a9f
kokeblom <98337374+kokeblom@users.noreply.github.com>
2021-04-30 10:57:18 +0000

    licence

--- build.ps1
+++ build.ps1
@@ -33,4 +33,4 @@ dotnet restore "$runningDirectory/src/DotNetCoreCryptography.sln"
 Assert-LastExecution -message "Error in restoring packages." -haltExecution $true
 
 Write-Host "\n\n*******************TESTING SOLUTION*******************"
-dotnet test "$runningDirectory/src/DotNetCoreCryptography.Tests/DotNetCoreCryptography.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutput
+dotnet test "$runningDirectory/src/DotNetCoreCryptography.Tests/DotNetCoreCryptography.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutput=Test
