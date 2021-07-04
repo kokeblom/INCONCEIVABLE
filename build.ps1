a7b613728c930aed9c869d34d7fe8c910f0a8202
kokeblom <98337374+kokeblom@users.noreply.github.com>
2021-07-04 07:41:13 +0000

    added initial tests

--- build.ps1
+++ build.ps1
@@ -37,4 +37,5 @@ dotnet test "$runningDirectory/src/DotNetCoreCryptography.Tests/DotNetCoreCrypto
 Assert-LastExecution -message "Error in test running." -haltExecution $true
 
 Write-Host "\n\n*******************BUILDING SOLUTION*******************"
-dotnet build "$runningDirectory/src/DotNetCoreCryptography.sln" --configuration releas
+dotnet build "$runningDirectory/src/DotNetCoreCryptography.sln" --configuration release
+As
