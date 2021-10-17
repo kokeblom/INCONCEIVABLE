338e41df43849d519639b7da773915e0e1bca022
kokeblom <98337374+kokeblom@users.noreply.github.com>
2021-10-17 17:55:04 +0000

    fix include order

--- build.ps1
+++ build.ps1
@@ -41,4 +41,4 @@ dotnet build "$runningDirectory/src/DotNetCoreCryptography.sln" --configuration 
 Assert-LastExecution -message "Error in building in release configuration" -haltExecution $true
 
 Write-Host "\n\n*******************PUBLISHING SOLUTION*******************"
-dotnet pack "$runningDirectory/src/DotNetCoreCryptographyCore/DotNetCoreCryptographyCore.csproj" --configuration release -o "$runningDirectory/artifacts/NuGet" /p:Package
+dotnet pack "$runningDirectory/src/DotNetCoreCryptographyCore/DotNetCoreCryptographyCore.csproj" --configuration release -o "$runningDirectory/artifacts/NuGet" /p:PackageVers
