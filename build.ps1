2ac4539b549807eef666b64115e7aee938d0340a
kokeblom <98337374+kokeblom@users.noreply.github.com>
2022-04-18 17:41:36 +0000

    removed unnecessary code

--- build.ps1
+++ build.ps1
@@ -46,4 +46,4 @@ Assert-LastExecution -message "Error in creating nuget packages.." -haltExecutio
 
 if ($true -eq $nugetPublish) 
 {
-    Write-Host "\n\n*******************PUBLISHING NUGET PA
+    Write-Host "\n\n*******************PUBLISHING NUGET PACKA
