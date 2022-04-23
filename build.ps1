faa3c382178b4d1084d431ccd990c4d865bc459b
kokeblom <98337374+kokeblom@users.noreply.github.com>
2022-04-23 22:26:51 +0000

    Pontoon: Update French (fr) localization of Test Pilot Website

--- build.ps1
+++ build.ps1
@@ -46,4 +46,4 @@ Assert-LastExecution -message "Error in creating nuget packages.." -haltExecutio
 
 if ($true -eq $nugetPublish) 
 {
-    Write-Host "\n\n*******************PUBLISHING NUGET PACKAGE**********
+    Write-Host "\n\n*******************PUBLISHING NUGET PACKAGE************
