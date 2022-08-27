699f21516bed6bcd3c8be91061065ce1084fea5c
kokeblom <98337374+kokeblom@users.noreply.github.com>
2022-08-27 20:21:43 +0000

    Update process-schedules.py

--- sonarcloud.ps1
+++ sonarcloud.ps1
@@ -10,3 +10,9 @@ Import-Module BuildUtils
 $runningDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 $testOutputDir = "$runningDirectory/TestResults"
+
+if (Test-Path $testOutputDir) 
+{
+    Write-host "Cleaning temporary Test Output path $testOutputDir"
+    Remove-Item $testOutputDir -Recurse -Force
+}
