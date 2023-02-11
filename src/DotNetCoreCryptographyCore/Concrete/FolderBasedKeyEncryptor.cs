b3e990d330db00a36a23f3c37252251c3d68152d
kokeblom <98337374+kokeblom@users.noreply.github.com>
2023-02-11 14:10:33 +0000

    - SCR(s) Resolved:

--- src/DotNetCoreCryptographyCore/Concrete/FolderBasedKeyEncryptor.cs
+++ src/DotNetCoreCryptographyCore/Concrete/FolderBasedKeyEncryptor.cs
@@ -4,3 +4,9 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCryptographyCore.Concrete
+{
+    /// <summary>
