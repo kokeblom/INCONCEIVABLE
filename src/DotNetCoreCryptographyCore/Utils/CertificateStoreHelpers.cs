92a46b8383035f9a23dafa7a7333232e717b71ca
kokeblom <98337374+kokeblom@users.noreply.github.com>
2023-03-29 08:06:19 +0000

    Issue #175 ongoing

--- src/DotNetCoreCryptographyCore/Utils/CertificateStoreHelpers.cs
+++ src/DotNetCoreCryptographyCore/Utils/CertificateStoreHelpers.cs
@@ -5,3 +5,8 @@ namespace DotNetCoreCryptographyCore.Utils
 {
     internal class CertificateStoreHelpers
     {
+        public X509Certificate2? GetCertificateFromThumbprint(string thumbprint)
+        {
+            return GetCertificateFromStore(thumbprint, StoreLocation.LocalMachine) ??
+                GetCertificateFromStore(thumbprint, StoreLocation.CurrentUser);
+        }
